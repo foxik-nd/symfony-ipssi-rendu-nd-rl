2ccfc8847aad659e2ce5b3856f3d072620287098
Robin <robinlandais75@yahoo.com>
2022-12-03 23:56:09 +0100

    add Roles + NavBar

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -7,11 +7,13 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Form\CallbackTransformer;
 
 class RegistrationFormType extends AbstractType
 {
@@ -56,7 +58,30 @@ class RegistrationFormType extends AbstractType
                 ],
                 'label' => 'Prénom'
             ])
-        ;
+            ->add('roles', ChoiceType::class, [
+                'required' => true,
+                'multiple' => false,
+                'expanded' => false,
+                'label' => 'Que souhaitez vous faire sur IDM ?',
+                'choices'  => [
+                    'Acheter' => 'ROLE_USER',
+                    'Vendre' => 'ROLE_SELLER',
+                    'Admin' => 'ROLE_ADMIN',
+                ],
+            ])
+            ;
+        // Data transformer
+        $builder->get('roles')
+            ->addModelTransformer(new CallbackTransformer(
+                function ($rolesArray) {
+                    // transform the array to a string
+                    return count($rolesArray) ? $rolesArray[0] : null;
+                },
+                function ($rolesString) {
+                    // transform the string back to an array
+                    return [$rolesString];
+                }
+            ));
     }
 
     public function configureOptions(OptionsResolver $resolver): void
