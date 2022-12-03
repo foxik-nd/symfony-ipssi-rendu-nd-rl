5871713e52a47bb6b488053aa3861d77c0512f64
Robin <robinlandais75@yahoo.com>
2022-12-03 19:56:29 +0100

    add: HomePage + Articles + Article

--- src/Controller/ContentController.php
+++ src/Controller/ContentController.php
@@ -5,14 +5,17 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Controller\ArticleController;
 
 class ContentController extends AbstractController
 {
     #[Route('/', name: 'app_content', methods: ['GET'])]
-    public function index(): Response
-    {
-        return $this->render('content/index.html.twig', [
-           'controller_name' => 'ContentController',
-        ]);
-    }
+    public function index()
+{
+    $response = $this->forward('App\Controller\ArticleController::last3');
+
+    // ... further modify the response or return it directly
+
+    return $response;
+}
 }
